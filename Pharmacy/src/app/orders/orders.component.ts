354b71900d33ef0582639150f87e698f53a544c0
Klaudia <gora.klaudia@gmail.com>
2019-05-25 21:10:25 +0200

    dd view for orders

--- Pharmacy/src/app/orders/orders.component.ts
+++ Pharmacy/src/app/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../login/login.service';
+import { Order } from './Order';
 
 @Component({
   selector: 'app-orders',
@@ -9,12 +10,27 @@ import { LoginService } from '../login/login.service';
 })
 export class OrdersComponent implements OnInit {
 
+  listOfOrders: Order[] = [];
+  orderToShow: Order;
+  showBox: Boolean;
+
+
   constructor(private router: Router, private httpLogin: LoginService) { }
 
   ngOnInit() {
+    this.showBox = false;
     if (this.httpLogin.token === '' || this.httpLogin.token === undefined) {
       this.router.navigate(['/login']);
     }
   }
 
+  reciveedOrder(i: Order) {
+    console.log(i);
+  }
+
+  showDetails(i: Order) {
+    console.log(i);
+    this.showBox = true;
+    this.orderToShow = i;
+  }
 }
